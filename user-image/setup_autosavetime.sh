d7d2fa138512c40b42b7bf94c91ff211fa581b67
Louis Rémus <15720130+louis-r@users.noreply.github.com>
2018-01-25 21:58:17 -0800

    Removed auxiliary sh file which caused Travis build to fail

--- user-image/setup_autosavetime.sh
+++
@@ -1,6 +0,0 @@
-# Install nb extensions
-pip install jupyter_contrib_nbextensions
-# We install, ignoring running servers
-jupyter contrib nbextensions install --sys-prefix --skip-running-check
-# Enable autosavetime which will by default disable autosave
-jupyter nbextension enable autosavetime/main
